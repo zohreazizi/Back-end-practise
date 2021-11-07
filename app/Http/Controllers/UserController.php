ab6e849fe239b9a2b6aeab0a925241f308c7cde6
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-11-08 03:18:48 +0330

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Throwable;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $data = $request->all();
+            $validator = Validator::make($data, [
+                'name' => 'required|max:55|unique:users',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|confirmed'
+            ]);
+
+            // if there are some error's, show them to user
+            if ($validator->fails()) {
+                return response()->json($validator->errors()->first(), 422);
+            }
+            $data['password'] = bcrypt($request->password);
+            $user = User::create($data);
+            $token = $user->createToken('NewToken')->accessToken;
+            return response()->json([
+                'token' => $token,
+                'token_type' => 'Bearer'
+            ]);
+        }catch (Throwable $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $token = auth()->user()->createToken('NewToken')->accessToken;
+            return response()->json([
+                'token' => $token,
+                'code' => 200
+            ]);
+        } else {
+            return response()->json(['error' => 'Unauthorized']);
+        }
+    }
+}
