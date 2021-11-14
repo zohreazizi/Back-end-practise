5ebe3366ec002bae2c0ac7880b89566001121a0d
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-11-14 14:42:45 +0330

    migrations created

---
+++ database/migrations/2021_11_13_170244_create_buses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('plate_number');
+            $table->string('company_name');
+            $table->integer('total_seats');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buses');
+
+    }
+}
