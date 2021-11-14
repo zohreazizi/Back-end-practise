76ff28803623b0f09b466b1e4e46d40f9b15e261
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-11-14 14:45:13 +0330

    FormRequest for storing buses created

---
+++ app/Http/Requests/StoreBusRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class StoreBusRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator)
+    {
+        return response()->json();
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:buses',
+            'plate_number' => 'required',
+            'total_seats' => 'required',
+            'company_name' => 'required'
+        ];
+    }
+
+}
