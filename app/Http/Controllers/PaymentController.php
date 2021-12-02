d0f750a16822e346d9515db7faf745de4b061dfa
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-12-02 15:10:26 +0330

    added a method to verify payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Payment;
-use Illuminate\Http\Request;
 use Pishran\Zarinpal\Zarinpal;
+use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
