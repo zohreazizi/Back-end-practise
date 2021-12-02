1f50ebbca44ea927fd3b0ec183bb6c3a300282c7
ZOHRE AZIZI <zohreh.azizi4@gmail.com>
2021-12-02 15:09:30 +0330

    added a method for pay

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,11 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Pishran\Zarinpal\Zarinpal;
 
 class PaymentController extends Controller
 {
-    public function pay(){
+    public function pay()
+    {
+        $zarinpal = \zarinpal()
+            ->amount(100)
+            ->request()
+            ->callbackUrl('http://127.0.0.1:7000/api/payment-gateway')
+            ->description('توضیحات تراکنش')
+            ->send();
 
+        if (!$zarinpal->success()) {
+            return response()->json($zarinpal->error()->message());
+        }
+
+        $authority = $zarinpal->authority();
+
+        return $zarinpal->redirect();
+    }
+
+    public function verify(Request $request)
+    {
+        $authority = \request()->query('Authority');
+        $amount = Payment::query()->where('authority', $authority)
+            ->value('amount');
+        if (!$amount) {
+            return null;
+        }
+
+        $response = \zarinpal()
+            ->amount($amount)
+            ->verification()
+            ->authority($authority)
+            ->send();
+        if (!$response->success()) {
+            return response()->json($response->error()->message());
+        }
+
+        return response()->json($response->referenceId());
     }
 }
