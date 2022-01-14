dad60d9e0ddc107f200a52b69df2ec3aad348756
zohre <zohreh.azizi4@gmail.com>
2022-01-14 18:50:37 +0330

    route fixed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Auth-Token');
-//header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
 
 use Illuminate\Support\Facades\Route;
 
